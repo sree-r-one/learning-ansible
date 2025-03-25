674a2d3585e853b7c1880d05a351b6109f0fcad8
sree <sreeraj_ec@hotmail.com>
2025-03-25 13:27:10 +0800

    update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,14 +3,9 @@ provider "azurerm" {
   subscription_id = var.subscription_id
 }
 
-resource "azurerm_resource_group" "main" {
-  name     = var.resource_group_name
-  location = var.location
-}
-
 resource "azurerm_container_registry" "acr" {
   name                = var.acr_name
-  resource_group_name = azurerm_resource_group.main.name
+  resource_group_name = var.resource_group_name
   location            = var.location
   sku                 = "Basic"
   admin_enabled       = true
